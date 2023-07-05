52676c08f567bd9662c4d357e4fccb9c1530503c
ackintosh <sora.akatsuki@gmail.com>
2023-07-05 09:11:47 +0900

    Install NAT rules

--- network.py
+++ network.py
@@ -27,12 +27,17 @@ from mininet.util import irange
 from mininet.node import Node
 
 class RestrictedConeNAT(Node):
+    # nat1
+    # debug:  192.168.1.0/24 nat1-eth1 False {'ip': '10.0.0.4/8', 'inetIntf': 'nat1-eth0'}
+    # nat2
+    # debug:  192.168.2.0/24 nat2-eth1 False {'ip': '10.0.0.5/8', 'inetIntf': 'nat2-eth0'}
     def __init__( self, name, subnet='10.0/8',
                   localIntf=None, flush=False, **params):
         """Start NAT/forwarding between Mininet and external network
            subnet: Mininet subnet (default 10.0/8)
            flush: flush iptables before installing NAT rules"""
         super( RestrictedConeNAT, self ).__init__( name, **params )
+        print('debug: __init__:', subnet, localIntf, flush, params)
 
         self.subnet = subnet
         self.localIntf = localIntf
@@ -61,6 +66,7 @@ class RestrictedConeNAT(Node):
     def config( self, **params ):
         """Configure the NAT and iptables"""
 
+        print('debug: config:', params)
         if not self.localIntf:
             self.localIntf = self.defaultIntf()
 
@@ -73,21 +79,50 @@ class RestrictedConeNAT(Node):
             self.cmd( 'sysctl net.ipv4.ip_forward=0' )
             self.cmd( 'iptables -F' )
             self.cmd( 'iptables -t nat -F' )
-            # Create default entries for unmatched traffic
-            self.cmd( 'iptables -P INPUT ACCEPT' )
-            self.cmd( 'iptables -P OUTPUT ACCEPT' )
-            self.cmd( 'iptables -P FORWARD DROP' )
+            # self.cmd( 'iptables -P INPUT ACCEPT' )
+            # self.cmd( 'iptables -P OUTPUT ACCEPT' )
+            # self.cmd( 'iptables -P FORWARD DROP' )
+
+        self.cmd( 'iptables -t nat -F' )
+
+        # Create default entries for unmatched traffic
+        self.cmd( 'iptables -P INPUT ACCEPT' )
+        self.cmd( 'iptables -P OUTPUT ACCEPT' )
+        self.cmd( 'iptables -P FORWARD DROP' )
 
         # Install NAT rules
-        self.cmd( 'iptables -I FORWARD',
-                  '-i', self.localIntf, '-d', self.subnet, '-j DROP' )
-        self.cmd( 'iptables -A FORWARD',
-                  '-i', self.localIntf, '-s', self.subnet, '-j ACCEPT' )
-        self.cmd( 'iptables -A FORWARD',
-                  '-o', self.localIntf, '-d', self.subnet, '-j ACCEPT' )
+        # 内部ネットワーク（192.168.0.0/24）から送られてきたパケットの送信元アドレスを、パブリックインターフェイス（eth0）のIPアドレスに変更する（NATを行う）
+        # iptables -t nat -A POSTROUTING -o eth0 -s 192.168.0.0/24 -j MASQUERADE
         self.cmd( 'iptables -t nat -A POSTROUTING',
-                  '-s', self.subnet, "'!'", '-d', self.subnet,
-                  '-j MASQUERADE' )
+                  '-o', params.get('inetIntf'), '-s', self.subnet, '-j MASQUERADE' )
+
+        # パブリックインターフェイス（eth0）から送られてきたパケットを内部ネットワーク（192.168.0.0/24）に転送
+        # iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+        self.cmd( 'iptables -A FORWARD',
+                  '-i', params.get('inetIntf'), '-o', self.localIntf, '-m state --state RELATED,ESTABLISHED -j ACCEPT' )
+
+        # 内部ネットワーク（192.168.0.0/24）から送られてきたパケットをパブリックインターフェイス（eth0）に転送
+        # iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
+        self.cmd( 'iptables -A FORWARD',
+                  '-i', self.localIntf, '-o', params.get('inetIntf'), '-j ACCEPT' )
+
+        # iptables -t nat -A POSTROUTING -o eth1 -p udp -j SNAT --to-source "public IP"
+        # self.cmd( 'iptables -t nat -A POSTROUTING',
+        #           '-o', params.get('inetIntf'), '-p udp', '--to-source', params.get('ip'), '-j SNAT' )
+
+        # iptables -t nat -A PREROUTING -i eth1 -p udp -j DNAT --to-destination "private IP"
+        # self.cmd( 'iptables -t nat -A PREROUTING',
+        #           '-i', params.get('inetIntf'), '-p udp', '--to-destination', params.get('ip'), '-j DNAT' )
+
+        # self.cmd( 'iptables -I FORWARD',
+        #           '-i', self.localIntf, '-d', self.subnet, '-j DROP' )
+        # self.cmd( 'iptables -A FORWARD',
+        #           '-i', self.localIntf, '-s', self.subnet, '-j ACCEPT' )
+        # self.cmd( 'iptables -A FORWARD',
+        #           '-o', self.localIntf, '-d', self.subnet, '-j ACCEPT' )
+        # self.cmd( 'iptables -t nat -A POSTROUTING',
+        #           '-s', self.subnet, "'!'", '-d', self.subnet,
+        #           '-j MASQUERADE' )
 
         # Instruct the kernel to perform forwarding
         self.cmd( 'sysctl net.ipv4.ip_forward=1' )
