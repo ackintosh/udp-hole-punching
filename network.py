52b847b87824bec5593dea91bbe56ba54ff9c318
ackintosh <sora.akatsuki@gmail.com>
2023-07-07 09:15:02 +0900

    code formatting

--- network.py
+++ network.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-natnet.py: Example network with NATs
+Network with Restricted Cone NATs
 
            h0
            |
@@ -14,7 +14,6 @@ natnet.py: Example network with NATs
    s1              s2
     |              |
    h1              h2
-
 """
 
 from mininet.topo import Topo
@@ -25,118 +24,160 @@ from mininet.cli import CLI
 from mininet.util import irange
 from mininet.node import Node
 
+
 class RestrictedConeNAT(Node):
-    def __init__( self, name, subnet='10.0/8',
-                  localIntf=None, flush=False, **params):
-        """Start NAT/forwarding between Mininet and external network
-           subnet: Mininet subnet (default 10.0/8)
-           flush: flush iptables before installing NAT rules"""
-        super( RestrictedConeNAT, self ).__init__( name, **params )
-        print('debug: __init__:', subnet, localIntf, flush, params)
+    def __init__(self, name, subnet='10.0/8', localIntf=None, **params):
+        super(RestrictedConeNAT, self).__init__(name, **params)
+        print('debug: __init__:', subnet, localIntf, params)
 
         self.subnet = subnet
         self.localIntf = localIntf
-        self.flush = flush
-        self.forwardState = self.cmd( 'sysctl -n net.ipv4.ip_forward' ).strip()
+        self.forwardState = self.cmd('sysctl -n net.ipv4.ip_forward').strip()
 
-    def setManualConfig( self, intf ):
+    def setManualConfig(self, intf):
         """Prevent network-manager/networkd from messing with our interface
            by specifying manual configuration in /etc/network/interfaces"""
         cfile = '/etc/network/interfaces'
         line = '\niface %s inet manual\n' % intf
         try:
-            with open( cfile ) as f:
+            with open(cfile) as f:
                 config = f.read()
         except IOError:
             config = ''
-        if ( line ) not in config:
-            info( '*** Adding "' + line.strip() + '" to ' + cfile + '\n' )
-            with open( cfile, 'a' ) as f:
-                f.write( line )
+        if line not in config:
+            info('*** Adding "' + line.strip() + '" to ' + cfile + '\n')
+            with open(cfile, 'a') as f:
+                f.write(line)
             # Probably need to restart network manager to be safe -
             # hopefully this won't disconnect you
-            self.cmd( 'service network-manager restart || netplan apply' )
+            self.cmd('service network-manager restart || netplan apply')
 
     # pylint: disable=arguments-differ
-    def config( self, **params ):
+    def config(self, **params):
         """Configure the NAT and iptables"""
 
-        print('debug: config:', params)
         if not self.localIntf:
             self.localIntf = self.defaultIntf()
 
-        self.setManualConfig( self.localIntf )
+        self.setManualConfig(self.localIntf)
 
         # Now we can configure manually without interference
         super(RestrictedConeNAT, self).config(**params)
 
-        self.cmd( 'iptables -F' )
-        self.cmd( 'iptables -t nat -F' )
+        self.cmd('iptables -F')
+        self.cmd('iptables -t nat -F')
 
         # Create default entries for unmatched traffic
-        self.cmd( 'iptables -P INPUT ACCEPT' )
-        self.cmd( 'iptables -P OUTPUT ACCEPT' )
-        self.cmd( 'iptables -P FORWARD ACCEPT' )
+        self.cmd('iptables -P INPUT ACCEPT')
+        self.cmd('iptables -P OUTPUT ACCEPT')
+        self.cmd('iptables -P FORWARD ACCEPT')
 
         # Install NAT rules
         # iptables -t nat -A POSTROUTING -o nat1-eth0 -p udp -j SNAT --to-source <public ip>
-        self.cmd( 'iptables -t nat -A POSTROUTING',
-                  '-o', params.get('inetIntf'), '-p udp', '-j SNAT', '--to-source', params.get('ip').split('/')[0], verbose=True )
+        self.cmd(
+            'iptables -t nat -A POSTROUTING',
+            '-o',
+            params.get('inetIntf'),
+            '-p udp',
+            '-j SNAT',
+            '--to-source',
+            params.get('ip').split('/')[0],
+            verbose=True
+        )
 
         # iptables -t nat -A PREROUTING -i eth1 -p udp -j DNAT --to-destination <private ip>
-        self.cmd( 'iptables -t nat -A PREROUTING',
-                  '-i', params.get('inetIntf'), '-p udp', '-j DNAT', '--to-destination', params.get('hostIp').split('/')[0], verbose=True )
+        self.cmd(
+            'iptables -t nat -A PREROUTING',
+            '-i',
+            params.get('inetIntf'),
+            '-p udp',
+            '-j DNAT',
+            '--to-destination',
+            params.get('hostIp').split('/')[0],
+            verbose=True
+        )
 
         # iptables -A FORWARD -i eth1 -p udp -m state --state ESTABLISHED,RELATED -j ACCEPT
-        self.cmd( 'iptables -A FORWARD',
-                  '-i', params.get('inetIntf'), '-p udp', '-m state', '--state ESTABLISHED,RELATED', '-j ACCEPT', verbose=True )
+        self.cmd(
+            'iptables -A FORWARD',
+            '-i',
+            params.get('inetIntf'),
+            '-p udp',
+            '-m state',
+            '--state ESTABLISHED,RELATED',
+            '-j ACCEPT',
+            verbose=True
+        )
 
         # iptables -A FORWARD -i eth1 -p udp -m state --state NEW -j DROP
-        self.cmd( 'iptables -A FORWARD',
-                  '-i', params.get('inetIntf'), '-p udp', '-m state', '--state NEW', '-j DROP', verbose=True )
+        self.cmd(
+            'iptables -A FORWARD',
+            '-i',
+            params.get('inetIntf'),
+            '-p udp',
+            '-m state',
+            '--state NEW',
+            '-j DROP',
+            verbose=True
+        )
 
         # Instruct the kernel to perform forwarding
-        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+        self.cmd('sysctl net.ipv4.ip_forward=1' )
 
-    def terminate( self ):
-        self.cmd( 'iptables -F' )
-        self.cmd( 'iptables -t nat -F' )
+    def terminate(self ):
+        self.cmd('iptables -F')
+        self.cmd('iptables -t nat -F')
         # Put the forwarding state back to what it was
-        self.cmd( 'sysctl net.ipv4.ip_forward=%s' % self.forwardState )
+        self.cmd('sysctl net.ipv4.ip_forward=%s' % self.forwardState)
         super(RestrictedConeNAT, self).terminate()
+
 
 class NetworkTopology(Topo):
     # pylint: disable=arguments-differ
-    def build(self, n=2, **_kwargs ):
+    def build(self, n=2, **_kwargs):
         # set up inet switch
-        inetSwitch = self.addSwitch('s0')
-        # add inet host
-        inetHost = self.addHost('h0')
-        self.addLink(inetSwitch, inetHost)
+        inet_switch = self.addSwitch('s0')
+        # add bootstrap node
+        inet_host = self.addHost('h0')
+        self.addLink(inet_switch, inet_host)
 
         # add local nets
         for i in irange(1, n):
-            inetIntf = 'nat%d-eth0' % i
-            localIntf = 'nat%d-eth1' % i
-            localIP = '192.168.%d.1' % i
-            localSubnet = '192.168.%d.0/24' % i
-            hostIp = '192.168.%d.100/24' % i
-            natParams = { 'ip' : '%s/24' % localIP }
+            inet_intf = 'nat%d-eth0' % i
+            local_intf = 'nat%d-eth1' % i
+            local_ip = '192.168.%d.1' % i
+            local_subnet = '192.168.%d.0/24' % i
+            host_ip = '192.168.%d.100/24' % i
+            nat_params = {'ip': '%s/24' % local_ip}
+
             # add NAT to topology
-            nat = self.addNode('nat%d' % i, cls=RestrictedConeNAT, subnet=localSubnet,
-                               inetIntf=inetIntf, localIntf=localIntf, hostIp=hostIp)
+            nat = self.addNode(
+                'nat%d' % i,
+                cls=RestrictedConeNAT,
+                subnet=local_subnet,
+                inetIntf=inet_intf,
+                localIntf=local_intf,
+                hostIp=host_ip
+            )
+
+            # add local switch to topology
             switch = self.addSwitch('s%d' % i)
+
             # connect NAT to inet and local switches
-            self.addLink(nat, inetSwitch, intfName1=inetIntf)
-            self.addLink(nat, switch, intfName1=localIntf, params1=natParams)
+            self.addLink(nat, inet_switch, intfName1=inet_intf)
+            self.addLink(nat, switch, intfName1=local_intf, params1=nat_params)
+
             # add host and connect to local switch
-            host = self.addHost('h%d' % i,
-                                ip=hostIp,
-                                defaultRoute='via %s' % localIP)
+            host = self.addHost(
+                'h%d' % i,
+                ip=host_ip,
+                defaultRoute='via %s' % local_ip
+            )
             self.addLink(host, switch)
 
+
 def run():
-    "Create network and run the CLI"
+    """Create network and run the CLI"""
     topo = NetworkTopology()
     net = Mininet(topo=topo, waitConnected=True)
     net.start()
